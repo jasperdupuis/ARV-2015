1c5b79d076ade870f30859ffcfada628e677d258
webste2r <nicholas.campbell@dal.ca>
2015-07-11 16:14:08 -0300

    Create uart.c

---
+++ uart.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include "encoderlib.h"
+
+static int uart_putchar(char c, FILE *stream);
+static int uart_getchar(FILE *stream);
+FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+FILE mystdin = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+
+static int uart_putchar(char c, FILE *stream)
+{
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0 = c;
+	return 0;
+}
+static int uart_getchar(FILE *stream)
+{
+	loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
+	return UDR0;
+}
+void init_uart(void)
+{
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+	UBRR0 = 7;
+	stdout = &mystdout;
+	stdin = &mystdin;
+}
